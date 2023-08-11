a295fea5a7ed71a5e5e02ad46ad49d821a282f32
XH0PE <pecioso999@gmail.com>
2023-08-11 10:03:54 -0500

    fase inicial

--- proyecto-final-node/src/routers/index.js
+++ proyecto-final-node/src/routers/index.js
@@ -1,8 +1,9 @@
 import { Router, request } from "express";
 const router = Router()
 
-router.get('/', (req, res) => res.render('index', {title: 'Home'}))
-router.get('/contact', (req, res) => res.render('contact', {title: 'Contact'}))
-router.get('/about', (req, res) => res.render('about', {title: 'About'}))
+router.get('/', (req, res) => res.render('juego', {title: 'Juego'}))
+router.get('/inf', (req, res) => res.render('info', {title: 'Requisitos'}))
+router.get('/work', (req, res) => res.render('workshop', {title: 'Workshop'}))
+router.get('/about', (req, res) => res.render('acerca', {title: 'Acerca de'}))
 
 export default router
