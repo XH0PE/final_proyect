08477beb51a18f5a668420f4f42f617b09b39146
Diego <pecioso999@gmail.com>
2023-08-11 20:44:54 -0500

    final stage

--- proyecto-final-node/src/routers/index.js
+++ proyecto-final-node/src/routers/index.js
@@ -3,7 +3,7 @@ const router = Router()
 
 router.get('/', (req, res) => res.render('juego', {title: 'Juego'}))
 router.get('/inf', (req, res) => res.render('info', {title: 'Requisitos'}))
-router.get('/work', (req, res) => res.render('workshop', {title: 'Workshop'}))
+router.get('/cap', (req, res) => res.render('capturas', {title: 'Capturas'}))
 router.get('/about', (req, res) => res.render('acerca', {title: 'Acerca de'}))
 
 export default router
